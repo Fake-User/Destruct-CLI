9a01a8822b6151bcf4ef5f03795145035ae9c576
Fake.User <russellboswell@protonmail.com>
2025-02-07 16:42:30 -0500

    updated mod structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 #![allow(dead_code, unused_variables)]
-
-mod sync;
-mod path;
-mod console;
+mod util;
+mod cmd;
 
 #[tokio::main]
 async fn main(){
@@ -12,24 +10,24 @@ async fn main(){
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_line_number(false)
-        .with_ansi(false)
-        .without_time()
         .with_target(false)
         .with_level(false)
+        .with_ansi(false)
+        .without_time()
         .init();
 
-    console::help();
+    util::console::help();
 
     'main_loop: loop {
         let mut cmd = String::new();
         match std::io::stdin().read_line(&mut cmd){
             Ok(_) => {
                 match cmd.trim(){
-                    "sync" => {sync::sync().await},
-                    "path" => {path::set_path()},
-                    "help" => {console::help()},
-                    "quit" => {console::clear(); break 'main_loop;},
-                    _ => {console::help()}
+                    "quit" => {util::console::clear(); break 'main_loop;},
+                    "sync" => {util::sync::sync().await},
+                    "path" => {cmd::path::set_path()},
+                    "help" => {util::console::help()},
+                    _ => {util::console::help()}
                 }
             }
             Err(e) => {println!("{}", e)}
